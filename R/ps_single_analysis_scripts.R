d8b61994b152f0f57c5ca18debb16efa1ff29341
Oliver Eales <odeales@gmail.com>
2024-06-17 15:51:09 +1000

    Update ps_single_analysis_scripts.R

--- R/ps_single_analysis_scripts.R
+++ R/ps_single_analysis_scripts.R
@@ -52,6 +52,57 @@ ps_single_incidence <- function(ps_fit,
 }
 
 
+# Returns modelled incidence including day of the week effect
+ps_single_incidence_dow <- function(ps_fit,
+                                X,
+                                num_days = length(X), time_labels,
+                                week_effect, DOW,
+                                days_per_knot = 5, spline_degree = 3){
+  
+  
+  X <- as.numeric(X)
+  
+  knots <- get_knots(X, days_per_knot = days_per_knot, spline_degree = spline_degree)
+  num_knots <- length(knots)
+  
+  
+  num_basis <- num_knots + spline_degree - 1
+  num_data <- length(X)
+  
+  B_true <- splines::bs(seq(knots[1], knots[length(knots)], 1), knots = knots[2:(length(knots)-1)], degree=spline_degree, intercept = TRUE)
+  B_true <- t(predict(B_true, X))
+  
+  
+  post <- rstan::extract(ps_fit)
+  
+  a <- array(data=NA, dim=c(nrow(post$a), num_days))
+  
+  for(k in 1:nrow(post$a)){
+    a[k,] <- as.array(post$a[k,]) %*% B_true
+  }
+  
+  df <- data.frame()
+  
+  for(i in 1:num_days){
+    total <- matrix(data=0, nrow=1, ncol=nrow(a))
+    
+    quan<- quantile(exp(a[,i])* week_effect*post$day_of_week_simplex[,DOW[i]], c(0.5,0.025, 0.25, 0.75, 0.975))
+    row <- data.frame(time = time_labels[i],
+                      t_step = i,
+                      y=quan[[1]],
+                      lb_50 = quan[[3]],
+                      ub_50 = quan[[4]],
+                      lb_95 = quan[[2]],
+                      ub_95 = quan[[5]])
+    df <- rbind(df, row)
+    
+  }
+  
+  df
+  
+  
+}
+
 # Returns data.frame() of modeled growth rates
 ps_single_growth_rate <- function(ps_fit,
                            X,
