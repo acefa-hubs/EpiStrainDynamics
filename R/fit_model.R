dcfe2efb9e42dc9055f184b7663217c75095bae2
Saras Windecker <smwindecker@users.noreply.github.com>
2024-10-23 16:00:06 +1000

    add time as a data item in data list. reprogram so that fit_model calls time in data list

--- R/fit_model.R
+++ R/fit_model.R
@@ -11,7 +11,8 @@ fit_model <- function (data,
   model <- get_model_info(pathogen_type, pathogen_names, method)
 
   # data into format for stan
-  Y <- data$cases
+  cases <- data$cases
+  time <- data$time
   main_pathogens <- t(matrix(data = do.call(c, data$main_pathogens), ncol = 3))
   if (pathogen_type == 'influenzaA_subtypes') {
     influenzaA_subtypes <- t(matrix(
@@ -22,19 +23,19 @@ fit_model <- function (data,
 
   if (method == 'p-spline') {
     # Calculate the locations of equally spaced knots
-    knots <- get_knots(dfS2$t, days_per_knot = 5, spline_degree = 3)
+    knots <- get_knots(time, days_per_knot = 5, spline_degree = 3)
   }
 
   # fit stan model
   if (model == 'rw_influenza') {
     fit <- rw_influenza_stan(
-      num_data = length(Y),
+      num_data = length(cases),
       num_path = length(pathogen_names),
-      Y = Y,
+      Y = cases,
       P1 = main_pathogens,
       P2 = influenzaA_subtypes,
       week_effect = 1,
-      DOW = (df1$time %% 1) + 1,
+      DOW = (time %% 1) + 1,
       cov_structure = 1,
       iter = iter,
       warmup = warmup,
@@ -42,17 +43,31 @@ fit_model <- function (data,
   }
   if (model == 'ps_mp') {
     fit <- ps_mp_stan(
-      num_data = length(Y),
+      num_data = length(cases),
       num_path = length(pathogen_names),
       num_knots = length(knots),
       knots = knots,
       spline_degree = 3,
-      Y = Y,
-      X = dfS2$t,
+      Y = cases,
+      X = time,
       P = main_pathogens,
       week_effect = 1,
-      DOW = (dfS2$t %% 1) + 1,
+      DOW = (time %% 1) + 1,
       cov_structure = 0,
+      iter = iter,
+      warmup = warmup,
+      chains = chains)
+  }
+  if (model == 'ps_single') {
+    ps_single_fit <- ps_single_stan(
+      num_data = length(cases),
+      num_knots = length(knots),
+      knots = knots,
+      spline_degree = 3,
+      Y = cases,
+      X = time,
+      week_effect = 7,
+      DOW = (time %% 7) + 1,
       iter = iter,
       warmup = warmup,
       chains = chains)
