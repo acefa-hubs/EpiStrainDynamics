296b083236994069e82ea13188c235333a09ed0a
Saras Windecker <smwindecker@users.noreply.github.com>
2024-10-29 21:23:37 +1000

    finish renaming from main to component pathogens to match paper

--- R/get_pathogen_info.R
+++ R/get_pathogen_info.R
@@ -4,12 +4,12 @@ get_pathogen_info <- function (data) {
     message('Ensure that component influenza A is listed first.')
     pathogen_names <- c(
       names(data$influenzaA_subtypes),
-      names(data$main_pathogens)[-1]
+      names(data$component_pathogens)[-1]
     )
   }
   if (!'influenzaA_subtypes' %in% names(data)) {
     pathogen_type <- 'other'
-    pathogen_names <- names(data$main_pathogens)
+    pathogen_names <- names(data$component_pathogens)
   }
   out <- list(pathogen_type = pathogen_type,
               pathogen_names = pathogen_names)
