f8d59efdacd53539cd3c87d15600d87dddc293e8
Saras Windecker <smwindecker@users.noreply.github.com>
2025-03-17 15:37:42 +1000

    building fails with square brackets in the roxgygen

--- R/ps_influenza_stan.R
+++ R/ps_influenza_stan.R
@@ -15,16 +15,17 @@
 #' @param week_effect Number of distinct days in day of week effect. 1 = single
 #'  effect shared by all days (essentially no DOW), 7 = each day a unique effect
 #' @param DOW integer of day of the week
-#' @param cov_structure 0 is tau[1], 1 is tau[num_path], 2 is Sigma[num_path, num_path]
+#' @param cov_structure 0 is tau of 1, 1 is tau of
+#'    num_path, 2 is Sigma with two dimensions: num_path, num_path.
 #' @param ... Arguments passed to `rstan::sampling` (e.g. iter, chains).
 #'
 #' @return An object of class `stanfit` returned by `rstan::sampling`
 #'
 ps_influenza_stan <- function (num_data, num_knots, num_path,
-                               knots, spline_degree,
-                               Y, P1, P2, X,
-                               week_effect, DOW,
-                               cov_structure = c(0, 1, 2), ...) {
+                                   knots, spline_degree,
+                                   Y, P1, P2, X,
+                                   week_effect, DOW,
+                                   cov_structure = c(0, 1, 2), ...) {
 
   standata <- list(num_data = num_data,
                    num_knots = num_knots,
