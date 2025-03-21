fb7f44939e51dfde79b827733d66d1d13c5857ea
Saras Windecker <smwindecker@users.noreply.github.com>
2025-03-21 15:29:58 +1000

    amend names of data types and smoothing structure default setting

--- R/fit_model.R
+++ R/fit_model.R
@@ -12,12 +12,12 @@ fit_model <- function (constructed_model,
 }
 
 
-fit <- function(constructed_model, iter, warmup, chains) UseMethod("fit")
+fit <- function (constructed_model, iter, warmup, chains) UseMethod("fit")
 
 # fit stan model
 fit.rw_influenza <- function (constructed_model, iter, warmup, chains) {
 
-  cases <- constructed_model$data$total_cases
+  cases <- constructed_model$data$case_timeseries
   pathogen_names <- constructed_model$pathogen_names
 
   fit_object <- rw_influenza_stan(
@@ -25,7 +25,7 @@ fit.rw_influenza <- function (constructed_model, iter, warmup, chains) {
     num_path = length(pathogen_names),
     Y = cases,
     P1 = constructed_model$data$component_pathogens,
-    P2 = constructed_model$data$influenzaA_subtypes,
+    P2 = constructed_model$data$influenzaA_subtyped,
     week_effect = constructed_model$model_params$week_effect,
     DOW = constructed_model$model_params$DOW,
     cov_structure = constructed_model$model_params$cov_structure,
@@ -41,82 +41,86 @@ fit.rw_influenza <- function (constructed_model, iter, warmup, chains) {
 
   return(out)
 }
-#
-# fit.ps_influenza <- function () {
-#   fit <- ps_influenza_stan(
-#     num_data = length(cases),
-#     num_path = length(pathogen_names),
-#     num_knots = length(knots),
-#     knots = knots,
-#     spline_degree = spline_degree,
-#     Y = cases,
-#     X = time,
-#     P1 = component_pathogens,
-#     P2 = influenzaA_subtypes,
-#     week_effect = week_effect,
-#     DOW = DOW,
-#     cov_structure = cov_structure,
-#     noise_structure = noise_structure,
-#     iter = iter,
-#     warmup = warmup,
-#     chains = chains)
-# }
-#
-# if (model == 'rw_mp') {
-#   fit <- rw_mp_stan(
-#     num_data = length(cases),
-#     num_path = length(pathogen_names),
-#     Y = cases,
-#     P = component_pathogens,
-#     week_effect = week_effect,
-#     DOW = DOW,
-#     cov_structure = cov_structure,
-#     noise_structure = noise_structure,
-#     iter = iter,
-#     warmup = warmup,
-#     chains = chains
-#   )
-# }
-# if (model == 'ps_mp') {
-#   fit <- ps_mp_stan(
-#     num_data = length(cases),
-#     num_path = length(pathogen_names),
-#     num_knots = length(knots),
-#     knots = knots,
-#     spline_degree = spline_degree,
-#     Y = cases,
-#     X = time,
-#     P = component_pathogens,
-#     week_effect = week_effect,
-#     DOW = DOW,
-#     cov_structure = cov_structure,
-#     noise_structure = noise_structure,
-#     iter = iter,
-#     warmup = warmup,
-#     chains = chains
-#   )
-# }
-#
-# if (model == 'ps_single') {
-#   fit <- ps_single_stan(
-#     num_data = length(cases),
-#     num_knots = length(knots),
-#     knots = knots,
-#     spline_degree = spline_degree,
-#     Y = cases,
-#     X = time,
-#     week_effect = week_effect,
-#     DOW = DOW,
-#     iter = iter,
-#     warmup = warmup,
-#     chains = chains
-#   )
-# }
-# if (model == 'rw_single') {
-#   fit <- rw_single_stan(
-#     num_data = length(cases),
-#     Y = cases,
-#     week_effect = week_effect,
-#     DOW = DOW
-#   )
-# }
+
+fit.ps_influenza <- function (constructed_model, iter, warmup, chains) {
+
+  cases <- constructed_model$data$case_timeseries
+  pathogen_names <- constructed_model$pathogen_names
+
+  fit <- ps_influenza_stan(
+    num_data = length(cases),
+    num_path = length(pathogen_names),
+    num_knots = length(knots),
+    knots = knots,
+    spline_degree = spline_degree,
+    Y = cases,
+    X = time,
+    P1 = component_pathogens,
+    P2 = influenzaA_subtypes,
+    week_effect = week_effect,
+    DOW = DOW,
+    cov_structure = cov_structure,
+    noise_structure = noise_structure,
+    iter = iter,
+    warmup = warmup,
+    chains = chains)
+}
+
+if (model == 'rw_mp') {
+  fit <- rw_mp_stan(
+    num_data = length(cases),
+    num_path = length(pathogen_names),
+    Y = cases,
+    P = component_pathogens,
+    week_effect = week_effect,
+    DOW = DOW,
+    cov_structure = cov_structure,
+    noise_structure = noise_structure,
+    iter = iter,
+    warmup = warmup,
+    chains = chains
+  )
+}
+if (model == 'ps_mp') {
+  fit <- ps_mp_stan(
+    num_data = length(cases),
+    num_path = length(pathogen_names),
+    num_knots = length(knots),
+    knots = knots,
+    spline_degree = spline_degree,
+    Y = cases,
+    X = time,
+    P = component_pathogens,
+    week_effect = week_effect,
+    DOW = DOW,
+    cov_structure = cov_structure,
+    noise_structure = noise_structure,
+    iter = iter,
+    warmup = warmup,
+    chains = chains
+  )
+}
+
+if (model == 'ps_single') {
+  fit <- ps_single_stan(
+    num_data = length(cases),
+    num_knots = length(knots),
+    knots = knots,
+    spline_degree = spline_degree,
+    Y = cases,
+    X = time,
+    week_effect = week_effect,
+    DOW = DOW,
+    iter = iter,
+    warmup = warmup,
+    chains = chains
+  )
+}
+if (model == 'rw_single') {
+  fit <- rw_single_stan(
+    num_data = length(cases),
+    Y = cases,
+    week_effect = week_effect,
+    DOW = DOW
+  )
+}
