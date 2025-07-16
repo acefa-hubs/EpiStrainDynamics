0173c23847420d77a89c0be7e62749c405d30d75
Saras Windecker <smwindecker@users.noreply.github.com>
2025-07-16 13:44:08 +1000

    document new functions

--- R/construct_model.R
+++ R/construct_model.R
@@ -1,8 +1,13 @@
-# this approach leaves less room for a user to put in the wrong data or select options that are irrelevant.
-# makes the choies they should make more explicit rather than needing to follow branching paths of
-# documentation perfectly and us relying on lots of stop>errors or messages.
-
-# also separates the decisions needed for data prep and decisions needed to model parameterisation.
+#' Construct model
+#'
+#' @param method either random_walk() or p_spline()
+#' @param pathogen_structure either single(), multiple(), or subtyped()
+#' @param dow_effect logical whether to incorporate a day of week model
+#'
+#' @returns a list containing the data, the model parameters, and pathogen
+#'  names
+#' @export
+#'
 construct_model <- function (method,
                              pathogen_structure,
                              dow_effect = NULL) {
@@ -53,6 +58,13 @@ construct_model <- function (method,
   return(model_input)
 }
 
+#' Single pathogen structure
+#'
+#' @param case_timeseries total case data
+#'
+#' @returns formatted list with pathogen structure and data
+#' @export
+#'
 single <- function (case_timeseries) {
   model_inputs <- list(
     pathogen_structure = 'single',
@@ -65,6 +77,21 @@ single <- function (case_timeseries) {
   model_inputs
 }
 
+#' Multiple pathogen structure
+#'
+#' @param case_timeseries total case data
+#' @param component_pathogen_timeseries named list of each pathogen that has
+#'   timeseries of case counts
+#' @param smoothing_structure either 'shared' (all pathogens have the same;
+#'   tau[1]), 'independent' (each pathogen has completely independent smoothing
+#'   structure; tau[number of pathogens]), or 'correlated' (smoothing structure is
+#'   correlated among pathogens Sigma[number of pathogens, number of pathogens])
+#' @param observation_noise either 'observation_noise_only' (only includes
+#'   observation noise - the same between pathogens) or pathogen_specific_noise'
+#'   (includes noise in individual pathogens as well)
+#' @returns named list including pathogen_structure, data, and model_params
+#' @export
+#'
 multiple <- function (case_timeseries,
                       component_pathogen_timeseries,
                       smoothing_structure = c(
@@ -109,6 +136,27 @@ multiple <- function (case_timeseries,
   model_inputs
 }
 
+#' Subtyped pathogen structure
+#'
+#' @param case_timeseries total case data
+#' @param influenzaA_unsubtyped_timeseries case timeseries data for unsubtyped
+#'   influenzaA
+#' @param influenzaA_subtyped_timeseries case timeseries data for subtyped
+#'   influenzaA
+#' @param other_pathogen_timeseries other pathogen case timeseries
+#' @param smoothing_structure either 'shared' (all pathogens have the same;
+#'   tau[1]), 'independent' (each pathogen has completely independent smoothing
+#'   structure; tau[number of pathogens]), or 'correlated' (smoothing structure is
+#'   correlated among pathogens Sigma[number of pathogens, number of pathogens])
+#' @param observation_noise either 'observation_noise_only' (only includes
+#'   observation noise - the same between pathogens) or pathogen_specific_noise'
+#'   (includes noise in individual pathogens as well)
+#'
+#' @importFrom rlang arg_match
+#'
+#' @returns named list including pathogen_structure, data, and model_params
+#' @export
+#'
 subtyped <- function (case_timeseries,
                       influenzaA_unsubtyped_timeseries,
                       influenzaA_subtyped_timeseries,
@@ -169,6 +217,12 @@ subtyped <- function (case_timeseries,
   model_inputs
 }
 
+#' Random walk method
+#'
+#' @returns list with method identified as random walk
+#' @export
+#'
+#' @examples random_walk()
 random_walk <- function () {
   out <- list(
     method = 'random-walk'
@@ -176,6 +230,16 @@ random_walk <- function () {
   return(out)
 }
 
+#' P-spline method
+#'
+#' @param spline_degree polynomial degree of the individual spline segments
+#'   used to construct the overall curve
+#' @param days_per_knot number of days for each knot
+#'
+#' @returns list with method and model parameters
+#' @export
+#'
+#' @examples p_spline()
 p_spline <- function (spline_degree = 3,
                       days_per_knot = 3) {
 
