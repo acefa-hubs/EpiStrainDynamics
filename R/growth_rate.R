a83a12f70b25a3f410fb7855d49cd8fc5106044c
Saras Windecker <smwindecker@users.noreply.github.com>
2025-08-04 15:26:45 +1000

    overhaul and restructure of post fitting functions

--- R/growth_rate.R
+++ R/growth_rate.R
@@ -1,100 +1,87 @@
-#' Calculate growth rate
+#' Generic Method for Growth Rate Analysis
 #'
-#' @param fitted_model fitted model output created with `fit_model`
+#' S3 generic for computing growth rates from fitted models
 #'
-#' @returns growth rate calculation
+#' @param fitted_model Fitted model object with appropriate class
+#' @param ... Additional arguments passed to methods
+#' @return Data frame with growth rate analysis results
 #' @export
-#'
-calculate_growth_rate <- function (fitted_model) {
-  gr <- growth_rate(fitted_model)
-  return(gr)
+growth_rate <- function(fitted_model, ...) {
+  UseMethod("growth_rate")
 }
 
-#' Calculate growth rate
-#'
-#' @param fitted_model fitted model output created with `fit_model`
-#'
-#' @returns growth rate calculation
-#'
-growth_rate <- function (fitted_model) UseMethod("growth_rate")
-
-#' @exportS3Method
-growth_rate.rw <- function (fitted_model) {
-
-  class(out) <- 'EpiStrainDynamics.fit'
-
-  return(out)
+#' @rdname growth_rate
+#' @export
+growth_rate.ps <- function(fitted_model, ...) {
+  compute_multi_pathogen(fitted_model, 2, 'growth_rate',
+                         threshold = 0, use_splines = TRUE)
 }
 
-#' @exportS3Method
-growth_rate.ps <- function (fitted_model) {
-
-  class(out) <- 'EpiStrainDynamics.fit'
-
-  return(out)
+#' @rdname growth_rate
+#' @export
+growth_rate.rw <- function(fitted_model, ...) {
+  compute_multi_pathogen(fitted_model, 2, 'growth_rate',
+                         threshold = 0, use_splines = FALSE)
 }
 
-#' @exportS3Method
-growth_rate.rw_single <- function (fitted_model) {
-
-  class(out) <- 'EpiStrainDynamics.fit'
-
-  return(out)
+#' @rdname growth_rate
+#' @export
+growth_rate.ps_single <- function(fitted_model, ...) {
+  compute_single_pathogen(fitted_model, 2, 'growth_rate',
+                          threshold = 0, use_splines = TRUE)
 }
 
-#' @exportS3Method
-growth_rate.ps_single <- function (fitted_model) {
-
-  fit <- fitted_model$fit
-  days_per_knot <- fitted_model$constructed_model$model_params$days_per_knot
-  spline_degree <- fitted_model$constructed_model$model_params$spline_degree
-  time <- fitted_model$constructed_model$data$time
-  time_labels <- fitted_model$constructed_model$data$time
-
-  B_true <- predict_B_true(time, days_per_knot, spline_degree)
-
-  post <- rstan::extract(fit)
-
-  num_days <- length(time)
-  a <- array(data = NA, dim = c(nrow(post$a), num_days))
-
-  for(k in 1:nrow(post$a)){
-    a[k,] <- as.array(post$a[k,]) %*% B_true
-  }
-
-  ## multiple ex
-  # single_path_gr_internal <- function (obj) {do.call(rbind, lapply(2:num_days, gr_internal, obj, time_labels))}
-  #
-  # apply(a, 2, single_path_gr_internal)
-
-
-  df <- do.call(rbind, lapply(2:num_days, gr_internal, obj, time_labels))
-
-
-  df
-
+#' @rdname growth_rate
+#' @export
+growth_rate.rw_single <- function(fitted_model, ...) {
+  compute_single_pathogen(fitted_model, 2, 'growth_rate',
+                          threshold = 0, use_splines = FALSE)
 }
 
-#' Internal growth rate function
+# =====================
+# CALCULATION FUNCTIONS
+# =====================
+
+#' Calculate Growth Rate for Single Pathogen Model
 #'
-#' @param iter loop id
-#' @param obj posterior samples
-#' @param time_labels time values
+#' Computes log-difference in incidence for single pathogen models
 #'
-#' @returns row processed for given iter
-gr_internal <- function (iter, obj, time_labels) {
+#' @param a Array of log-incidence posterior samples [samples, time]
+#' @param time_idx Integer time index
+#' @param pathogen_idx NULL (unused but required for interface consistency)
+#' @param post Posterior samples object (unused but required for interface consistency)
+#' @param components Model components (unused but required for interface consistency)
+#' @return Vector of growth rate posterior samples
+calc_growth_single <- function(a, time_idx, pathogen_idx, post, components) {
+  a[, time_idx] - a[, time_idx - 1]
+}
 
-  quan <- quantile(obj[, iter] - obj[, (iter - 1)],
-                   c(0.5, 0.025, 0.25, 0.75, 0.975))
-  prop <- length(obj[, iter][(obj[, iter] - obj[, (iter - 1)]) > 0]) /
-    length(obj[, iter] - obj[, (iter - 1)])
-  row <- data.frame(time = time_labels[iter],
-                    t_step = iter,
-                    y = quan[[1]],
-                    lb_50 = quan[[3]],
-                    ub_50 = quan[[4]],
-                    lb_95 = quan[[2]],
-                    ub_95 = quan[[5]],
-                    prop = prop)
-  return(row)
+#' Calculate Growth Rate for Individual Pathogen
+#'
+#' Computes log-difference in incidence for a specific pathogen
+#'
+#' @param a Array of log-incidence posterior samples [samples, pathogens, time]
+#' @param time_idx Integer time index
+#' @param pathogen_idx Integer pathogen index
+#' @param post Posterior samples object (unused but required for interface consistency)
+#' @param components Model components (unused but required for interface consistency)
+#' @return Vector of growth rate posterior samples
+calc_growth_individual <- function(a, time_idx, pathogen_idx, post, components) {
+  a[, pathogen_idx, time_idx] - a[, pathogen_idx, time_idx - 1]
+}
+
+#' Calculate Total Growth Rate Across All Pathogens
+#'
+#' Computes log-difference in total incidence across all pathogens
+#'
+#' @param a Array of log-incidence posterior samples [samples, pathogens, time]
+#' @param time_idx Integer time index
+#' @param pathogen_idx NULL (unused but required for interface consistency)
+#' @param post Posterior samples object (unused but required for interface consistency)
+#' @param components Model components (unused but required for interface consistency)
+#' @return Vector of total growth rate posterior samples
+calc_growth_total <- function(a, time_idx, pathogen_idx, post, components) {
+  total_i <- rowSums(exp(a[, , time_idx]))
+  total_im1 <- rowSums(exp(a[, , time_idx - 1]))
+  log(total_i) - log(total_im1)
 }
