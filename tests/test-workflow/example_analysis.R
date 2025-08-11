737c5dd7e66d66a3c6e36592339fd0a106a27165
Saras Windecker <smwindecker@users.noreply.github.com>
2025-08-11 12:18:35 +1000

    modify prep example analysis to incorporate new changes

--- tests/test-workflow/example_analysis.R
+++ tests/test-workflow/example_analysis.R
@@ -4,8 +4,13 @@
 library(ggplot2)
 library(rstan)
 library(EpiStrainDynamics)
-# source('R/growth_rate.R')
-# source('R/predict_B_true.R')
+source('../for_comparison/previous_growth_rate.R')
+source('../for_comparison/Rt_old.R')
+source('../for_comparison/incidence_old.R')
+
+gammaDist <- function(b, n, a) (b**n) * (a**(n-1)) * exp(-b*a) / gamma(n)
+rw_gi_dist <- function(x) gammaDist(b=1, n=1, x)
+ps_gi_dist <- function(x) gammaDist(b=2, n=2, x)
 
 # Set some stan settings
 rstan::rstan_options(auto_write = TRUE)
@@ -24,12 +29,12 @@ influenza_data$week <- as.Date(influenza_data$week)
 influenza_data <- influenza_data[influenza_data$week < max_date &
                                    influenza_data$week >= min_date, ]
 influenza_data <- influenza_data[order(influenza_data$week), ]
-influenza_data$time <- seq(1, nrow(influenza_data))
 
 rw_subtyped_mod <- construct_model(
   method = random_walk(),
   pathogen_structure = subtyped(
     case_timeseries = influenza_data$ili,
+    time = influenza_data$week,
     influenzaA_unsubtyped_timeseries = influenza_data$inf_A,
     other_pathogen_timeseries = list(
       influenzaB = influenza_data$inf_B,
@@ -50,9 +55,25 @@ rw_subtyped_fit <- fit_model(
   warmup = 1000,
   chains = 3
 )
-rw_subtyped_gr <- rw_growth_rate(
-  rw_subtyped_fit
-)
+rw_subtyped_gr <- growth_rate(rw_subtyped_fit)
+rw_subtyped_rt <- Rt(rw_subtyped_fit,
+                     gi_dist = rw_gi_dist)
+rw_subtyped_inc <- incidence(rw_subtyped_fit, dow = FALSE)
+plot(rw_subtyped_inc)
+plot(rw_subtyped_gr)
+plot(rw_subtyped_rt)
+
+rw_subtyped_inc_dow <- incidence(rw_subtyped_fit, dow = TRUE)
+rw_subtyped_prop <- proportion(rw_subtyped_fit,
+                               numerator_combination = c('influenzaA.H3N2',
+                                                         'influenzaA.H1N1',
+                                                         'influenzaB'),
+                               denominator_combination = c('influenzaA.H3N2',
+                                                           'influenzaA.H1N1',
+                                                           'influenzaB',
+                                                           'other'))
+
+
 
 ps_subtyped_mod <- construct_model(
   method = p_spline(),
@@ -78,16 +99,42 @@ ps_subtyped_fit <- fit_model(
   warmup = 1000,
   chains = 3
 )
-# ps_subtyped_gr <- ps_growth_rate(
-#   ps_influenza,
-#   influenza_data_list$time,
-#   time_labels = influenza_data_list$time
-# )
+ps_subtyped_gr_orig <- ps_growth_rate_orig(ps_subtyped_fit) |>
+  dplyr::arrange(pathogen)
+ps_subtyped_gr <- ps_growth_rate(ps_subtyped_fit) |>
+  dplyr::arrange(pathogen) |>
+  as.data.frame()
+ps_subtyped_gr <- ps_subtyped_gr[,colnames(ps_subtyped_gr_orig)]
+
+ps_subtyped_rt_orig <- ps_Rt_orig(ps_subtyped_fit,
+                                  gi_dist = ps_gi_dist) |>
+  dplyr::arrange(pathogen)
+ps_subtyped_rt <- ps_Rt(ps_subtyped_fit,
+                        gi_dist = ps_gi_dist) |>
+  dplyr::arrange(pathogen) |>
+  as.data.frame()
+ps_subtyped_rt <- ps_subtyped_rt[,colnames(ps_subtyped_rt_orig)]
+
+ps_subtyped_incidence <- ps_incidence(ps_subtyped_fit)
+ps_subtyped_DOW_incidence <- ps_incidence_dow(ps_subtyped_fit)
+
+# ps_sub_prop <- proportion(ps_subtyped_fit$fit,
+#                         X = NULL,
+#                         method = c('rw', 'ps'),
+#                         num_days = length(X),
+#                         time_labels,
+#                         days_per_knot = 5,
+#                         spline_degree = 3,
+#                         num_path = 4,
+#                         comb_num = list(c(1,2,3), c(1,2), c(3), c(1), c(2)),
+#                         comb_den = list(c(1,2,3,4), c(1,2,3,4), c(1,2,3,4), c(1,2), c(1,2) ),
+#                         comb_names = c("Influenza", "Influenza A", "Influenza B", "H3N2", "H1N1")
 
 
 # Example 2 "mp" models
 # Fitting the model 'mid-season', only include first 140 days
 # Load some simulated data
+# use for the sarscov2
 sim_data_raw <- read.csv('tests/test-workflow/example_data/simulated_data1.csv')
 sim_data <- sim_data_raw[sim_data_raw$t < 140, ]
 
@@ -111,7 +158,30 @@ rw_multiple_fit <- fit_model(
   warmup = 300,
   chains = 3
 )
+rw_multiple_gr_orig <- rw_growth_rate_orig(rw_multiple_fit) |>
+  dplyr::arrange(pathogen)
+rw_multiple_gr <- growth_rate(rw_multiple_fit) |>
+  dplyr::arrange(pathogen) |>
+  as.data.frame()
+rw_multiple_gr <- rw_multiple_gr[,colnames(rw_multiple_gr_orig)]
 
+rw_multiple_incidence <- rw_incidence(rw_subtyped_fit)
+
+# sarscov2 <-
+ps_multiple_mod <- construct_model(
+  method = p_spline(spline_degree = 3, days_per_knot = 5),
+  pathogen_structure = multiple(
+    case_timeseries = sim_data$y,
+    component_pathogen_timeseries = list(
+      influenzaA.H3N2 = sim_data$H3N2,
+      influenzaA.H1N1 = sim_data$H1N1,
+      influenzaB = sim_data$B
+    ),
+    smoothing_structure = 'independent',
+    observation_noise = 'observation_noise_only'
+  ),
+  dow_effect = TRUE
+)
 ps_multiple_mod <- construct_model(
   method = p_spline(spline_degree = 3, days_per_knot = 5),
   pathogen_structure = multiple(
@@ -132,11 +202,15 @@ ps_multiple_fit <- fit_model(
   warmup = 300,
   chains = 3
 )
-# ps_mp_gr <- ps_growth_rate(
-#   ps_mp,
-#   sim_data_list$time,
-#   time_labels = sim_data_list$time
-# )
+ps_multiple_gr_orig <- ps_growth_rate_orig(ps_multiple_fit) |>
+  dplyr::arrange(pathogen)
+ps_multiple_gr <- ps_growth_rate(ps_multiple_fit) |>
+  dplyr::arrange(pathogen) |>
+  as.data.frame()
+ps_multiple_gr <- ps_multiple_gr[,colnames(ps_multiple_gr_orig)]
+
+ps_multiple_incidence <- ps_incidence(ps_multiple_fit)
+ps_multiple_DOW_incidence <- ps_incidence_dow(ps_multiple_fit)
 
 # Penalised-spline model, multiple-pathogens, fit up to day 140 of simulated
 # dataset to demonstrate utility during flu-season/pandemic
@@ -166,8 +240,8 @@ ps_single_mod <- construct_model(
   method = p_spline(),
   pathogen_structure = single(
     case_timeseries = covid_data$metric_value
-  ),
-  dow_effect = TRUE
+  )#,
+  # dow_effect = FALSE
 )
 ps_single_fit <- fit_model(
   ps_single_mod,
@@ -175,8 +249,35 @@ ps_single_fit <- fit_model(
   warmup = 300,
   chains = 3
 )
-# ps_single_gr <- ps_single_growth_rate(
-#   ps_single,
-#   covid_data_list$time,
-#   time_labels = covid_data_list$time
-# )
+ps_single_mod_dow <- construct_model(
+  method = p_spline(),
+  pathogen_structure = single(
+    case_timeseries = covid_data$metric_value
+  ),
+  dow_effect = TRUE
+)
+ps_single_fit_dow <- fit_model(
+  ps_single_mod,
+  iter = 500,
+  warmup = 300,
+  chains = 3
+)
+ps_single_gr_orig <- ps_single_growth_rate_orig(ps_single_fit)
+ps_single_gr <- ps_single_growth_rate(ps_single_fit) |>
+  as.data.frame()
+ps_single_gr <- ps_single_gr[,colnames(ps_single_gr_orig)]
+
+ps_single_rt_orig <- ps_single_Rt_orig(ps_single_fit, gi_dist = ps_gi_dist)
+ps_single_rt <- ps_single_Rt(ps_single_fit, gi_dist = ps_gi_dist) |>
+  as.data.frame()
+ps_single_rt <- ps_single_rt[,colnames(ps_single_rt_orig)]
+
+ps_single_incidence_orig <- ps_single_incidence_orig(ps_single_fit)
+ps_single_incidence_dow_orig <- ps_single_incidence_dow(ps_single_fit_dow)
+ps_single_incidence <- ps_single_incidence(ps_single_fit) |>
+  dplyr::select(-prop) |>
+  as.data.frame()
+ps_single_incidence_dow <- ps_single_incidence(ps_single_fit_dow) |>
+  dplyr::select(-prop) |>
+  as.data.frame()
+
