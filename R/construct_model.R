96e9a4c406eaa8e74ea2cee7a79be4568b6e5674
Saras Windecker <smwindecker@users.noreply.github.com>
2025-10-01 10:52:39 +1000

    add parameters for prior definition to construct_model and remove from pathogen_structure functions

--- R/construct_model.R
+++ R/construct_model.R
@@ -2,17 +2,29 @@
 #'
 #' @param method either `random_walk()` or `p_spline()`
 #' @param pathogen_structure either `single()`, `multiple()`, or `subtyped()`
-#' @param dow_effect logical whether to incorporate a day of week model
-#' @param priors named list of priors
+#' @param smoothing_params argument is optional and defines the structure of the
+#'   smoothing terms including optionally setting the smoothing prior tau.
+#'   Created with `smoothing_structure()`. NULL option defaults to 'shared'
+#'   smoothing structure and default priors.
+#' @param dispersion_params argument is optional and defines priors for the
+#'   overdispersion parameter of the negative binomial likelihood for
+#'   the case timeseries. Created using `dispersion_structure()`. NULL option
+#'   uses default priors for phi.
+#' @param pathogen_noise logical whether individual pathogen counts have
+#'   additional gamma-distributed noise. Default is FALSE. Models with `single`
+#'   pathogen structure will be set to FALSE.
+#' @param dow_effect logical whether to incorporate a day of week model.
 #'
 #' @returns a list containing the data, the model parameters, and pathogen
 #'  names of class `EpiStrainDynamics.model`
 #' @export
 #'
+#' @srrstats {G1.4} uses `Roxygen2` documentation
 #' @srrstats {BS1.1} Code examples of how to enter both simple and more complex
 #'   data are here in the `construct_model()` documentation. Descriptive text
 #'   can be found in the `README.md`
-#' @srrstats {G1.4} uses `Roxygen2` documentation
+#' @srrstats {BS1.2, BS1.2c} Code examples of how to specify prior distributions.
+#'   Descriptive text can be found in the `README.md`
 #' @srrstats {BS2.15} checks that data has been input in correct form, and
 #'   provides an informative error message if not
 #'
@@ -24,32 +36,42 @@
 #'   time = sarscov2$date,
 #'   pathogen_name = "SARS-CoV-2"
 #' )
-#'
 #' mod <- construct_model(
 #'   method = method_obj,
 #'   pathogen_structure = pathogen_obj,
 #'   dow_effect = TRUE
 #' )
 #'
-#' # Alternatively, call the methods and pathogen structure inside the function
+#' # Specifying priors on tau and phi are optional
 #' mod <- construct_model(
+#'
 #'   method = p_spline(),
+#'
 #'   pathogen_structure = multiple(
 #'     case_timeseries = sarscov2$cases,
 #'     time = sarscov2$date,
+#'
 #'     component_pathogen_timeseries = list(
 #'       alpha = sarscov2$alpha,
 #'       delta = sarscov2$delta,
 #'       omicron = sarscov2$omicron,
-#'       other = sarscov2$other)),
-#'   dow_effect = FALSE
+#'       other = sarscov2$other)
+#'    ),
+#'
+#'    smoothing_params = smoothing_structure(
+#'       'independent', tau_mean = c(0, 0.1, 0.3, 0), tau_sd = rep(1, times = 4)),
+#'    dispersion_params = dispersion_structure(phi_mean = 0, phi_sd = 1),
+#'    pathogen_noise = FALSE,
+#'    dow_effect = TRUE
 #' )
+#'
 #'
 construct_model <- function(method,
                             pathogen_structure,
-                            dow_effect = FALSE,
-                            tau_priors = NULL,
-                            phi_priors = NULL) {
+                            smoothing_params = NULL,
+                            dispersion_params = NULL,
+                            pathogen_noise = FALSE,
+                            dow_effect = FALSE) {
 
   #' @srrstats {G2.1, G2.2, G5.8, G5.8b} assertions on types of inputs
   validate_class_inherits(
@@ -58,17 +80,24 @@ construct_model <- function(method,
   validate_class_inherits(
     pathogen_structure, 'EpiStrainDynamics.pathogen_structure'
   )
+  smoothing_params <- validate_smoothing_structure(
+    smoothing_params, pathogen_structure$pathogen_names)
+  if (!is.null(dispersion_params)) {
+    validate_class_inherits(dispersion_params, "EpiStrainDynamics.dispersion")
+  }
+
+  if (!is.logical(pathogen_noise) || length(pathogen_noise) != 1) {
+    cli::cli_abort(
+      "You provided {.var {pathogen_noise}} but `pathogen_noise` must be a single
+      logical value (TRUE or FALSE)"
+    )
+  }
   if (!is.logical(dow_effect) || length(dow_effect) != 1) {
     cli::cli_abort(
       "You provided {.var {dow_effect}} but `dow_effect` must be a single
       logical value (TRUE or FALSE)"
     )
   }
-  # priors
-  tau_priors <- validate_tau_priors(tau_priors, pathogen_structure)
-  phi_priors <- validate_phi_priors(phi_priors)
-  tau_priors_provided <- ifelse(is.null(tau_priors), 1, 2)
-  phi_priors_provided <- ifelse(is.null(phi_priors), 1, 2)
 
   # Extract model type
   model_type <- get_model_type(method$method,
@@ -85,13 +114,17 @@ construct_model <- function(method,
   pathogen_names <- pathogen_structure$pathogen_names
   component_pathogens <- pathogen_structure$data$component_pathogens %||% NULL
   influenzaA_subtyped <- pathogen_structure$data$influenzaA_subtyped %||% NULL
-  cov_structure <- pathogen_structure$model_params$cov_structure %||% NULL
-  noise_structure <- pathogen_structure$model_params$noise_structure %||% NULL
+  cov_structure <- get_cov_structure[smoothing_params$smoothing_type] %||% NULL
+  noise_structure <- as.numeric(pathogen_noise) %||% NULL
   spline_degree <- method$model_params$spline_degree %||% NULL
-  tau_mean <- tau_priors$mean %||% NULL
-  tau_sd <- tau_priors$sd %||% NULL
-  phi_mean <- phi_priors$mean %||% NULL
-  phi_sd <- phi_priors$sd %||% NULL
+
+  # priors
+  tau_priors_provided <- ifelse(is.null(tau_priors), 1, 2)
+  phi_priors_provided <- ifelse(is.null(phi_priors), 1, 2)
+  tau_mean <- smoothing_params$tau_priors$mean %||% NULL
+  tau_sd <- smoothing_params$tau_priors$sd %||% NULL
+  phi_mean <- dispersion_params$phi_priors$mean %||% NULL
+  phi_sd <- dispersion_params$phi_priors$sd %||% NULL
 
   standata <- list(num_data = length(cases),
                    Y = cases,
@@ -142,6 +175,10 @@ construct_model <- function(method,
     list(time_seq = time_seq),
     pathogen_structure$data
   )
+
+  # return priors
+  # tau_priors = smoothing_params$tau_priors,
+  # phi_priors = dispersion_params$phi_priors,
 
   # Construct final model input list
   model_input <- list(
@@ -241,4 +278,35 @@ get_knots <- function(X, days_per_knot = 3, spline_degree = 3) {
   knots <- seq(first_knot, final_knot, by = days_per_knot)
 
   return(knots)
+}
+
+#' Get covariance structure from assigned smoothing structure
+#'
+#' @param smoothing_structure either `shared` (all pathogens have the same
+#'   smoothing structure; \code{tau[1]}), `independent` (each pathogen with
+#'   independent smoothing structure; \code{tau[number of pathogens]}), or
+#'   `correlated` (smoothing structure is correlated among pathogens
+#'   \code{Sigma[number of pathogens, number of pathogens]}). Case-insensitive.
+#'
+#' @returns numeric value for covariance structure needed by stan models
+#' @noRd
+#'
+#' @srrstats {G1.4} uses `Roxygen2` documentation
+#' @srrstats {G1.4a} internal function specified with `@noRd`
+#'
+get_cov_structure <- function(smoothing_structure = c('shared',
+                                                      'independent',
+                                                      'correlated')) {
+
+  # Convert to numeric codes
+  cov_structure <- switch(smoothing_structure,
+                          'shared' = 0,
+                          'independent' = 1,
+                          'correlated' = 2,                            {
+                            cli::cli_abort("Invalid option provided: '{smoothing_structure}'.
+                                             Please choose 'shared', 'independent', or 'correlated'.")
+                          }
+  )
+
+  return(cov_structure)
 }
