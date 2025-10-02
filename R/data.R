8bc0e1906c3c854230094b71f069577f2c66ab5b
Saras Windecker <smwindecker@users.noreply.github.com>
2025-10-02 12:38:21 +1000

    remove documentation for influenza dataset and ensure all columns needed for examples are created (and no extras)

--- R/data.R
+++ R/data.R
@@ -36,20 +36,18 @@
 #' @srrstats {G1.4} uses `Roxygen2` documentation
 "sarscov2"
 
-#' THIS NEEDS TO BE EDITED
+#' World Health Organisation Global Influenza Programme for Australia
 #'
 #' Influenza-like illness data were retrieved from the World Health
-#' Organization’s Global Influenza Programme.16 We collated weekly data for
+#' Organization’s Global Influenza Programme. We collated weekly data for
 #' Australia from the week starting January 2, 2012 to the week starting
 #' December 25, 2023 inclusive. The data described: (1) the weekly number
 #' of cases of influenza-like illness; and (2) the weekly number of specimens
-#' positive for influenza by subtype (and the number of negative tests). We
-#' grouped the influenza specimens into: influenza A subtype not determined;
-#' influenza A H3N2; influenza A H1N1 (influenza A H1N1, influenza A H1N1pdm09);
-#' influenza B (influenza B Yamagata, influenza B Victoria, influenza B lineage
-#' not determined); and negative tests. Negative tests likely reflect
-#' individuals infected with other ILI-causing pathogens or other unknown
-#' causes which we do not have data to determine.
+#' positive for influenza by subtype. We grouped the influenza specimens into:
+#' influenza A subtype not determined; influenza A H3N2; influenza A H1N1
+#' (influenza A H1N1, influenza A H1N1pdm09); influenza B (influenza B Yamagata,
+#' influenza B Victoria, influenza B lineage not determined); and other (unknown
+#' causes which we do not have data to determine).
 #'
 #' @format ## `influenza`
 #' A data frame with 830 rows and 15 columns:
@@ -58,11 +56,9 @@
 #'   \item{week}{Date, between 1 January 2012 to week starting 1 March 2020}
 #'   \item{inf_A}{Integer, daily number of cases of unsubtyped influenza A}
 #'   \item{inf_B}{Integer, daily number of cases of influenza B}
-#'   \item{inf_all}{Integer, daily number of cases of }
-#'   \item{inf_neg}{Integer, daily number of cases of }
 #'   \item{inf_H3N2}{Integer, daily number of cases of influenza A subtype H3N2}
 #'   \item{inf_H1N1}{Integer, daily number of cases of influenza A subtype H1N1}
-#'   \item{num_spec}{Integer, number of specimens} ??
+#'   \item{other}{Integer, number of cases of unspecified influenza-like illness}
 #' }
 #' @source <https://www.who.int/teams/global-influenza-programme/surveillance-and-monitoring/influenza-surveillance-outputs>
 #' @srrstats {G5.1} Dataset `influenza` is exported and described in detail
