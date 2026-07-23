5926be72d43cdc059e928931e842382e3250c394
Saras Windecker <smwindecker@users.noreply.github.com>
2026-07-23 16:31:43 +0200

    Resolve faulty figure rendering by revising the fi path in the vignette itself and the knit options in the compile file (closes #45)
    knitr::knit() resolves fig.path relative to the working directory at call time, not relative to the input/output file. Since fig.path in the vignette is set relative to the vignette itself (e.g. "figures/..."), base.dir must be set to vignettes/ here so generated figures land in vignettes/figures/ instead of <wherever precompile.R was run from>/figures/.

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -1,4 +1,5 @@
 # Pre-compiled vignettes that are slow to run
-# Must manually move image files to vignettes/ after knit
+
+knitr::opts_knit$set(base.dir = "vignettes")
 knitr::knit("vignettes/Using-EpiStrainDynamics.Rmd.orig",
             output = "vignettes/Using-EpiStrainDynamics.Rmd")
